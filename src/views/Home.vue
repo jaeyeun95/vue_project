baeeb46ed0a00ee091ad93245ff87c0718f42b17
jaeyeunKim <jaeyeun95@solugate.com>
2021-10-07 09:05:22 +0900

    second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <button @click="getData">프레임워크 목록 가져오기</button>
+      <br>
+      <button @click="clickBtn()">클릭</button>
   </div>
 </template>
 
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue-resource@1.3.4"></script>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import axios from 'axios';
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods:{
+    getData(){
+      console.log("data")
+      axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+      .then(function(response){
+        console.log("response:: ",response);
+      })
+      .catch(err => console.log(err))
+    },
+    // getData(){
+    //   console.log("data")
+    //   this.$http.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+    //   .then(function(response){
+    //     console.log(response);
+    //     console.log(JSON.parse(response.data));
+    //   })
+    // },
+    clickBtn(event){
+      console.log(event);
+    }
   }
 }
 </script>
