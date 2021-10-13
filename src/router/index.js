00f542010da2efed0df3717ae44a05d7efa998ba
jaeyeun95 <jaeyeun95@gmail.com>
2021-10-13 13:10:57 +0900

    엑셀 다운로드

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/excel',
+    name: 'Excel',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Excel.vue')
+  },
+  {
     path: '/test',
     name: 'Test',
     // route level code-splitting
---
+++ src/views/Excel.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div>
+      <table id="table">
+        <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>age</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>1</td>
+          <td>aa</td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>bb</td>
+          <td>46</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>cc</td>
+          <td>84</td>
+        </tr>
+        </tbody>
+      </table>
+      <button @click="makeExcelFile4">Excel4</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    makeExcelFile4 () {
+      let tab_text = '<html xmlns:x="urn:schemas-microsoft-com:office:excel">'
+      tab_text += '<head><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">'
+      tab_text += '<xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>'
+      tab_text += '<x:Name>Test Sheet</x:Name>'
+      tab_text += '<x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions></x:ExcelWorksheet>'
+      tab_text += '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>'
+      tab_text += "<table>"
+      const temp = document.getElementById('table').innerHTML
+      console.log("temp :: "+temp)
+      tab_text += temp
+      tab_text += '</table></body></html>'
+      console.log(tab_text)
+      const fileName = 'exampleTable.xls'
+      const a_tag = document.createElement('a')
+      const blob = new Blob([tab_text], { type: 'application/vnd.ms-excel;charset=utf-8;' })
+      a_tag.href = window.URL.createObjectURL(blob)
+      a_tag.download = fileName
+      a_tag.click()
+    }
+  }
+}
+</script>
