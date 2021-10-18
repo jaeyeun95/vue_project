5a674808e5c3cd4c72d9ce570472e7dbfeb0b249
jaeyeun95 <jaeyeun95@gmail.com>
2021-10-18 09:01:37 +0900

    vue.js filter

--- src/router/index.js
+++ src/router/index.js
@@ -21,19 +21,18 @@ const routes = [
   {
     path: '/excel',
     name: 'Excel',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Excel.vue')
   },
   {
     path: '/test',
     name: 'Test',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/js',
+    name: 'js',
+    component: () => import(/* webpackChunkName: "about" */ '../views/JS.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/JS.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>filter</h1>
+        <h3>짝수</h3>
+        <li v-for="(n, index) in evenNumber" :key="index">{{ n }}</li>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:'JS',
+    data() {
+        return {
+            numbers: [1,2,3,4,5],
+        }
+    },
+    computed:{
+        evenNumber(){
+            // console.log(this.numbers.filter(item => console.log(item)))
+            return this.numbers.filter(item => item % 2 ===0);
+        }
+    },
+    methods: {
+        
+    },
+    
+}
+</script>
