552331b061ad871ae8f6300e71911b4903452418
jaeyeun95 <jaeyeun95@gmail.com>
2021-10-19 16:57:58 +0900

    computed 와 methods 사용 테스트

--- src/views/JS.vue
+++ src/views/JS.vue
@@ -2,7 +2,12 @@
     <div>
         <h1>filter</h1>
         <h3>짝수</h3>
+        <!-- computed 사용시 -->
+        <h4>computed</h4>
         <li v-for="(n, index) in evenNumber" :key="index">{{ n }}</li>
+        <!-- method 사용시 -->
+        <!-- <h4>methods</h4>
+        <li v-for="(item, index) in evenNumber2()" :key="index">{{ item }}</li> -->
         
     </div>
 </template>
@@ -21,7 +26,10 @@ export default {
         }
     },
     methods: {
-        
+        evenNumber2(){
+            // console.log(this.numbers.filter(item => console.log(item)))
+            return this.numbers.filter(item => item % 2 ===0);
+        }
     },
     
 }
