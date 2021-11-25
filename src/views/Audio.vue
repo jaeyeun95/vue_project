7d81c9e66cf35361e89926a571e6470c4c4e61dc
jaeyeun95 <jaeyeun95@gmail.com>
2021-11-25 16:20:33 +0900

    audio 재생 테스트 local 에 있는 wav파일 재생 성공 서버에서 가져오는데 프록시에서 에러

--- src/views/Audio.vue
+++ src/views/Audio.vue
@@ -5,32 +5,46 @@
             <source  id="audio_src"  type="audio/mpeg" :src="audiouSrc">
         </audio> -->
         <audio id="player" style="width:100%" controls="" preload="auto" ref="player" @click="play()">
-            <source type="audio/mp3"/>
+            <source type="audio/mp3" src="../audio/test.wav"/>
         </audio>
-        <!-- src="D:\\잡\\test.wav" -->
     </div>
 </template>
 <script>
-import axios from 'axios';
 export default {
     created(){
-        this.getAudio();
+        // this.getAudio();
+        this.test();
     },
     methods: {
-        getAudio(){
-            console.log('오디오 가져오기')
-            // this.$axios.post('/test/audio/download')
-            // this.$axios.post('http://localhost:9000/audio/download')
-            axios.post('/api/audio/download')
+        // getAudio(){
+        //     console.log('오디오 가져오기')
+        //     // this.$axios.post('/test/audio/download')
+        //     // this.$axios.post('http://localhost:9000/audio/download')
+        //     axios.post('/api/audio/download')
+        //     .then(res => console.log(res))
+        //     .catch(err => console.log(err))
+        // },
+        test(){
+            console.log('test 실행')
+            this.$axios.get('/api/board/list')
+            // this.$axios.get('http://localhost:9000/api/board/list')
             .then(res => console.log(res))
-            .catch(err => console.log(err))
+            .catch(err => console.log('에러',err))
         },
         play(){
             console.log('오디오 플레이', this.$refs.source.src)
             // this.$refs.source.src
-            let audio = new Audio('D:\\잡\\test.wav');
+            let audio = new Audio('../audio/test.wav');
             audio.play();
-        }
+
+
+        },
+        ttsPlay(base64){
+            var audio = new Audio("data:audio/wav;base64," + base64);
+            audio.controls = true;
+            document.body.appendChild(audio);
+            audio.play();
+        },
     },
 }
 </script>
